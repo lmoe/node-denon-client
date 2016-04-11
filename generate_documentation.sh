caec7b7c6d90cc0566d5d819145fae91de881428
lmoe <moe@codingart.de>
2016-04-11 18:41:56 +0200

    Update docs

--- generate_documentation.sh
+++ generate_documentation.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
 
+find . -name "*" -and -not -name "generate_documentation.sh" -and -not -name "package.json" -and -not -path '*/\.*' -and -not -path './node_modules*' -exec rm -rf {} \;
+
+
 yuidoc -t node_modules/yuidoc-lucid-theme -H node_modules/yuidoc-lucid-theme/helpers/helpers.js ../denon/lib -o ./
