d6ae4e458c4f913f73917c2b61168ba62408d0c0
lmoe <moe@codingart.de>
2016-04-11 17:46:39 +0200

    Remove files

--- Gulpfile.js
+++
@@ -1,8 +0,0 @@
-var gulp = require('gulp');
-var mocha = require('gulp-mocha');
-
-gulp.task('test', function () {
-	return gulp.src('./tests/*.js', {read: false})
-		// gulp-mocha needs filepaths so you can't have any plugins before it
-		.pipe(mocha({reporter: 'nyan'}));
-});
--- README.md
+++
@@ -1,2 +0,0 @@
-# node-denon-avr
-A handy node library which allows you to control and receive events of a Denon AVR.
