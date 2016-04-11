1519e91ed96656f4980ea2d2ad7d7e26d1e30f01
lmoe <moe@codingart.de>
2016-04-11 18:45:00 +0200

    Update docs

--- api.js
+++ api.js
@@ -4,6 +4,7 @@ YUI.add("yuidoc-meta", function(Y) {
         "DenonClient",
         "InputOptions",
         "MuteOptions",
+        "Options",
         "PowerOptions",
         "SurroundOptions",
         "VolumeOptions"
