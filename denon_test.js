bd3eab19735e93767c6d5d62a0f74e3245b70241
lmoe <moe@codingart.de>
2016-04-11 19:00:02 +0200

    Fix events

--- denon_test.js
+++ denon_test.js
@@ -1,4 +1,4 @@
-const Denon = require('./lib/denon');
+const Denon = require('./lib/denon_client');
 const denon = new Denon('192.168.2.252');
 const Options = require('./lib/options');
 
@@ -19,25 +19,5 @@ denon
     console.log(result);
   })
   .catch((error) => {
-    console.log("K")
     console.log(error);
   });
-
-/*    return denon.getVolume();
-  })
-  .then((result) => {
-    console.log(result);
-
-    return denon.getInput();
-  })
-  .then((result) => {
-    console.log(result);
-
-    return denon.getMute();
-  })
-  .then((result) => {
-    console.log(result);
-  })
-  .catch((error) => {
-    console.log(error);
-  })*/
