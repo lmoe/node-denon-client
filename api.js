4d2dcbdd007d16c1a8f4e570865ad8345324507f
lmoe <moe@codingart.de>
2016-04-11 17:48:33 +0200

    Initial docs commit

---
+++ api.js
@@ -0,0 +1,15 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "Denon",
+        "InputOptions",
+        "MuteOptions",
+        "PowerOptions",
+        "SurroundOptions",
+        "VolumeOptions"
+    ],
+    "modules": [],
+    "allModules": [],
+    "elements": []
+} };
+});
