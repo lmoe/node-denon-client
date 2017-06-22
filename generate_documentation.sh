794893a247a0ac20a089b9b75e78cf6079c25f5c
Lukas Möller <lmoe@tuta.io>
2017-06-22 22:36:24 +0200

    Update documentation

--- generate_documentation.sh
+++ generate_documentation.sh
@@ -3,4 +3,4 @@
 find . -name "*" -and -not -name "generate_documentation.sh" -and -not -name "package.json" -and -not -path '*/\.*' -and -not -path './node_modules*' -exec rm -rf {} \;
 
 
-yuidoc -t node_modules/yuidoc-lucid-theme -H node_modules/yuidoc-lucid-theme/helpers/helpers.js ../denon/lib -o ./
+yuidoc -t node_modules/yuidoc-lucid-theme -H node_modules/yuidoc-lucid-theme/helpers/helpers.js ../node-denon-client/lib -o ./
